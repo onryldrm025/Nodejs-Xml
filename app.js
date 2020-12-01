d72c85acbaad6fc1978d8bead3a9d0c1eb98e9e2
Onur Yıldırım <35971592+onryldrm025@users.noreply.github.com>
2020-12-01 16:19:50 +0300

    v1
    v1

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const xml2js = require('xml2js');
+const app = express();
+const path = require('path');
+
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+const parser = new xml2js.Parser();
+
+app.get('/', (req, res) => {
+    fs.readFile('./data.xml', (err, data) => {
+        parser.parseStringPromise(data).then(result => {
+            res.render('index', { datalist: result.Data.Kisi });
+        })
+    })
+})
+
+
+app.listen(8000, () => {
+    console.log('Server Çalısıyor')
+})
